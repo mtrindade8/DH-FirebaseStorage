79e03acc0497b5ef82160adc9b225b9d4c7900d9
Mateus <mateustrindade8@gmail.com>
2021-01-29 15:48:29 -0300

    Adição de splash screen

---
+++ app/src/main/java/com/example/desafiofirebase_digitalhousemobile/SplashScreenActivity.kt
@@ -0,0 +1,26 @@
+package com.example.desafiofirebase_digitalhousemobile
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private val scope = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        scope.launch {
+            delay(3000)
+
+            startActivity(Intent(application, LoginActivity::class.java))
+            finish()
+        }
+    }
+}
